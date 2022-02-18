91d55c8210d294ce1e3d4e55e015ba78c0f4b89f
lhd <chilee4650@naver.com>
2022-02-18 19:45:40 +0900

    JAVA_02.18_UP

--- work/TestAccount.java
+++ work/TestAccount.java
@@ -1,6 +1,31 @@
 class Account
 {
+	///Field
+		private String accNo;
+		private int balance;
+	///Constructor
+		public Account(){
+		}
+		public Account(String accNo){
+			this.accNo = accNo;
+			System.out.println(this.accNo+" 계좌가 개설되었습니다.");
+		}
+	///Method
+		public String getAccNo(int pwd){
+			if(pwd == 1234){
+				return this.accNo;
+			}else {
+				return 0;
+			}
+		}
 
+		public int getBalance(int pwd){
+			if(pwd == 1234){
+				return this.balance;
+			}else {
+				return 0;
+			}	
+		}
 }
 
 public class TestAccount
@@ -9,7 +34,20 @@ public class TestAccount
 	{
 		String accNo = null;
 		int balance = 0;
+		
+		Account accObj = new Account("078-3762-293");
+		accNo = accObj.getAccNo();
+		balance = accObj.getBalance();
+		System.out.println(accNo+" 계좌의 잔고는 "+balance+"만원 입니다.");
 
-
+		accObj.save(100);
+		balance = accObj.getBalance();
+		System.out.println(accObj.getAccNo()+"계좌의 잔고는"+balance+"만원입니다.");
+		
+		accObj.deposit(30);
+		balance = accObj.getBalance();
+		System.out.println(accNo+" 계좌의 잔고는 "+balance+"만원 입니다.");
+		
+	
 	}
 }
